1293946ae24a77516af905ac2ea8457468d0c907
kingbayron <kingbayron@gmail.com>
2021-06-29 23:36:20 -0400

    conexion

--- conexion.php
+++ conexion.php
@@ -1,6 +1,6 @@
 <?phP
     //Conexión a la base de datos php2 con el usuario webuser y contraseña php2
-    $conexion = mysqli_connect('localhost','webuser','php2','php2');
+    $conexion = mysqli_connect('localhost','root','holalol1','delyrio');
     if(mysqli_connect_error()){
         die('No se puede conectar a la base de datos'.mysqli_connect_error());
     }
